8bacb5e0f9f5c6df499424a690c0da91f6d7e16f
Tirth Dhamsania <tirthpatel3107@gmail.com>
2025-11-05 19:02:07 +0530

    Change the header file

--- src/components/new/Header.jsx
+++ src/components/new/Header.jsx
@@ -6,7 +6,7 @@ import "../../assets/styles/header.css";
 /* Header Component */
 const Header = ({ onMenuClick, isSidebarOpen }) => {
   return (
-    <header className="app-header">
+    <header className={`app-header ${isSidebarOpen ? 'sidebar-open' : ''}`}>
       <button 
         className={`hamburger-menu-button ${isSidebarOpen ? 'sidebar-open' : ''}`}
         onClick={onMenuClick}
